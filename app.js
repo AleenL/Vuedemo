3dd867287c5e9a9653b502f9528516deb2ca5f03
784757555@qq.com <784757555@qq.com>
2017-06-08 17:45:59 +0800

    update this todo

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ var app = new Vue({
   	},
 	newTodo:'',
 	todoList:[],
+  weatherList:[],
 	currentUser:null,
+  weatherShow:false,
    },
    created:function(){
    	this.currentUser = this.getCurrentUser();
@@ -47,7 +49,68 @@ var app = new Vue({
 			console.log('更新成功')
 		})   	
    	},
-
+    getWeather:function(){
+      if(!this.weatherShow){
+        
+        $(this.$refs.rightContent).css({'width':'150px','marginLeft':'-150px'})
+        $(this.$refs.middleContent).animate({marginRight:'150px'},880)  
+        this.weatherShow = true
+      }else{
+        $(this.$refs.rightContent).css({'width':'0px','marginLeft':'0px'})
+        $(this.$refs.middleContent).animate({marginRight:'0px'},580) 
+        this.weatherShow = true 
+        this.weatherShow = false
+      }
+        let _this = this
+        let cityUrl = 'http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js';
+        $.getScript(cityUrl, function(script, textStatus, jqXHR) {
+        let citytq = remote_ip_info.city
+        let url = "http://php.weather.sina.com.cn/iframe/index/w_cl.php?code=js&city=" + citytq + "&day=0&dfc=3";
+        $.ajax({
+          url : url,
+          dataType : "script",
+          scriptCharset : "gbk",
+          success : function(data) {
+            let _w = window.SWther.w[citytq][0]
+            let cloudtq = _w.d1 + _w.p1 + "级"
+            let tq =  _w.t1 + "℃～" + _w.t2 + "℃ " 
+            let logo = null,title=null
+              if(_w.s1.indexOf('阴') != -1){
+                logo="<i class='iconfont'>&#xe60a;</i>"
+                title='阴天，在不开灯的房间'
+              }else if(_w.s1.indexOf('雨') != -1){
+                logo="<i class='iconfont'>&#xe683;</i>"
+                title='下雨天，和巧克力更配哦'
+              }else if(_w.s1.indexOf('晴') != -1){
+                logo="<i class='iconfont'>&#xe683;</i>"
+                title='故事的小黄花，你还记得吗？'
+              }
+            _this.weatherList.push(
+            {city:citytq,
+            weath:_w.s1,
+            cloud:cloudtq,
+            qiwen:tq,
+            logo:logo,
+            title:title
+            })
+          }
+        })
+      })
+        this.weatherList=[];
+    },
+    chooseStyle:function(){
+      if(this.$refs.choose_style.style.width === '100px'){
+        this.$refs.choose_style.style.width = 0
+      }else{
+        this.$refs.choose_style.style.width = '100px'
+      }
+    },
+    chooseColor:function(e){
+      let backgroundColor = document.getElementsByClassName('background_color')
+      for(var i=0;i<backgroundColor.length;i++){
+       backgroundColor[i].style.backgroundColor=$(e.target).attr('data-color')
+      }
+    },
    	saveTodos:function(){
    		let dataString = JSON.stringify(this.todoList)
    		var AVTodos = AV.Object.extend('AllTodos');
@@ -72,6 +135,7 @@ var app = new Vue({
    		}
    	},
    	addTodo:function(){
+      if(this.newTodo == "") return
    		this.todoList.push({
    			title:this.newTodo,
    			createdAt:new Date(),
@@ -92,6 +156,7 @@ var app = new Vue({
    		user.signUp().then((loginedUser)=>{
    			this.currentUser = this.getCurrentUser()
    		},function(error){});
+
    	},
    	login:function(){
    		AV.User.logIn(this.formData.username, this.formData.password).then((loginedUser)=>{
